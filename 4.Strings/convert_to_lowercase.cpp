8be5da9991254bd77ef811d325fd449791855104
Anjali <anjalipal2912@gmail.com>
2025-09-23 00:44:07 +0530

    add updated code

---
+++ 4.Strings/convert_to_lowercase.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+void tolower(char word[] , int n){
+    for(int i = 0 ; i<n ; i++){
+        //current character
+        char ch = word[i];
+        if(ch>='a' && ch<='z'){
+            continue;
+        }else{
+            word[i] = ch - 'a';
+        }
+    }
+}
+int main(){
+    char word[] = "APPLE";
+    tolower(word,strlen(word));
+    cout<<word<<endl;
+    return 0;
+}
---
+++ 4.Strings/practice1.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char arr[5] = {'a','b','c','d','e'};
+    cout<<arr<<endl;
+    return 0;
+}
